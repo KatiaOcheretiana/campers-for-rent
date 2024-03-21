a1040d9eb94e7e86ea742b51068fe43b59dae115
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-03-21 22:08:35 +0200

    add settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { GlobalStyles } from "./styles/GlobalStyles.js";
-import { store } from "./redux/store.js";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { GlobalStyles } from './styles/GlobalStyles.js';
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
+      <BrowserRouter basename="/campers-for-rent">
+        <App />
+      </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
     <GlobalStyles />
