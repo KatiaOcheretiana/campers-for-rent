38db99f679b9a7a230edc2a61a1b3cc04e013f22
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-03-27 11:06:24 +0200

    add  meta.env.BASE_URL

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/campers-for-rent/">
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <App />
         </BrowserRouter>
       </PersistGate>
