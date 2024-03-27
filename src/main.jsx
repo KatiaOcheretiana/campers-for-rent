ea5cf607b814863e200dd1f19bbd9a4930ab92e9
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-03-27 10:59:11 +0200

    add 404 and update index.htm to fix  routes

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/campers-for-rent">
+        <BrowserRouter basename="/campers-for-rent/">
           <App />
         </BrowserRouter>
       </PersistGate>
